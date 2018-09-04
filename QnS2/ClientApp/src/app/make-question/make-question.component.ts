cb7f192751be61943da8d50697be480f2339f606
tduong2018 <40793513+tduong2018@users.noreply.github.com>
2018-09-04 22:03:42 +0700

    duong

--- QnS2/ClientApp/src/app/make-question/make-question.component.ts
+++ QnS2/ClientApp/src/app/make-question/make-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-make-question',
@@ -6,8 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./make-question.component.css']
 })
 export class MakeQuestionComponent implements OnInit {
+  
+  closeResult: string;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   ngOnInit() {
   }
