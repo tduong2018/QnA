02e5fbfa08ef8fec4f71788fadeb55ce01d4f819
Duong <tduong2018@gmail.com>
2018-09-05 17:00:49 +0700

    duong

--- QnS2/ClientApp/src/app/card-question/card-question.component.ts
+++ QnS2/ClientApp/src/app/card-question/card-question.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardQuestionComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed = true;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
+
+}
+
+interface card{
 
 }
