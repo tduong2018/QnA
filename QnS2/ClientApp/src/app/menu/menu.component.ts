180bd1b82271a42bced7c6607d99f1d15a482835
tduong2018 <40793513+tduong2018@users.noreply.github.com>
2018-09-06 18:35:12 +0700

    design home

--- QnS2/ClientApp/src/app/menu/menu.component.ts
+++ QnS2/ClientApp/src/app/menu/menu.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  public Categorys = ["All", "C#", "Angular", "ASP.NET MVC", "ASP.NET MVC Core"];
+  public Category: string;
   constructor() { }
 
   ngOnInit() {
+    this.Category = this.Categorys[0];
   }
 
+  public selectCategory(item){
+    this.Category = item;
+  }
 }
